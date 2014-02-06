e272425cf93684e67aee80fdee0da16ea2f7f392
hayk <hayk.baluyan@gmail.com>
2014-02-06 18:26:25 +0400

    commit FlattenList

---
+++ FlattenList/flattenList.py
@@ -0,0 +1,12 @@
+#complexity O(n+k) where n is number of total elements, k is number of nested lists
+#[[1,2],[3,4],5] -> n=5, k=2
+def flattenList(X):
+    Y=[];
+    for item in X:
+        if type(item) is list:
+            Y+=flattenList(item)
+        else:
+            Y+=[item]
+    return Y
+def start(L):
+    print(flattenList(L))
